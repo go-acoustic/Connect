0cad7c1ebe930284ecc97405c3027c13751b87f3
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2025-09-30 11:22:53 -0700

    September 30 2025 Connect Module 1.0.68 Connect Release
    September 30 2025 Connect Module 1.0.68
    Connect Release
    Bug Fixes:
    
    - None.
    
    Improvements:
    
    - Adjust ios library to support react native for npm plugin.
    - Adjust ios library to new minimal of 15.1.
    
    Our environment for this release:
    
    - Xcode 16.4
    - MacOS 15.5
    - iOS 15.1 to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- SDKs/iOS/Release/Connect.xcframework/ios-arm64/Connect.framework/Headers/ConnectApplication.h
+++ SDKs/iOS/Release/Connect.xcframework/ios-arm64/Connect.framework/Headers/ConnectApplication.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2024 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2025 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -14,11 +14,7 @@
 
 
 #import <UIKit/UIKit.h>
-//#if TEALEAF_USE_REACT
-//#import <TealeafReactNative/TLFApplication.h>
-//#else
-//#import <Tealeaf/TLFApplication.h>
-//#endif
+#import <Tealeaf/TLFApplication.h>
 
 @interface ConnectApplication : UIApplication
 
