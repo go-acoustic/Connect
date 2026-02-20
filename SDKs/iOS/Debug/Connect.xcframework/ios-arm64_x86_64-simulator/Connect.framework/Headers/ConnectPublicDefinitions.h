e4d84657b6184d267730f15fbe04510fa424017b
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2026-02-20 01:43:36 -0800

    Beta Connect build: 1.0.85
    Beta Connect Change Notes:
    Bug Fixes:
    
    - Default to system value for User-Agent header if it is not available
    
    Improvements:
    
    - Updated library using latest Xcode and iOS versions (26.2)
    
    Our environment for this release:
    
    - Xcode 26.2
    - MacOS 26.0
    - iOS 15.1 to 26.2
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- SDKs/iOS/Debug/Connect.xcframework/ios-arm64_x86_64-simulator/Connect.framework/Headers/ConnectPublicDefinitions.h
+++ SDKs/iOS/Debug/Connect.xcframework/ios-arm64_x86_64-simulator/Connect.framework/Headers/ConnectPublicDefinitions.h
@@ -19,7 +19,7 @@
 //#import "Tealeaf/ConnectPublicDefinitions.h"
 
 // Version
-#define CONNECT_FRAMEWORK_VER @"1.0.84" //ReleaseVersion.SprintVersion.MinorVersion
+#define CONNECT_FRAMEWORK_VER @"1.0.85" //ReleaseVersion.SprintVersion.MinorVersion
 #define kConnectCoreModule                              @"ConnectCoreModule"
 //#define kConfigurableItemEventLevels                    @"EventLevels"
 //#define kConfigurableItemTreatJsonDictionariesAsString  @"TreatJsonDictionariesAsString"
